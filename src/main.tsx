1e25aea70724730cc2b191ae432e560c82b15204
brownglobalofficial-glitch <brownglobalofficial@gmail.com>
2026-08-01 14:50:08 -0400

    Improve Studio navigation in src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import App from "./App";
 import "./styles.css";
 import "./brand.css";
 import "./account.css";
+import "./navigation.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode><App /></StrictMode>,
 );
+
