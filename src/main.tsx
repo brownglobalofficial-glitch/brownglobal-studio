db172c761ccc325aab5dbb813bf61e873d4197d3
brownglobalofficial-glitch <brownglobalofficial@gmail.com>
2026-08-01 17:31:06 -0400

    Add verified Capacitor mobile foundation for Studio

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { initializeMobileApp } from "./mobile";
 import "./styles.css";
 import "./brand.css";
 import "./account.css";
 import "./navigation.css";
+import "./mobile.css";
+
+void initializeMobileApp();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode><App /></StrictMode>,
